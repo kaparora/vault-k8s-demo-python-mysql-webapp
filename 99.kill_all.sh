50281527fb0a5b8a8f3fa32d003b67d9cfdab3b0
lomar92 <amarlojo@gmail.com>
2022-09-05 11:40:15 +0200

    patched demo

---
+++ 99.kill_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -o xtrace
+
+kubectl delete -f configmap.yaml
+kubectl delete -f webapp.yaml
+
+
+
+helm delete mysql
+helm delete vault
+kubectl delete serviceaccount webapp-sa
+kubectl delete sa vault-auth
+
+
+
+kubectl get pods mysql-client -o yaml mysql-client.yaml
+kubectl delete -f mysql.client.yaml
+kubectl delete pvc data-mysql-0
+rm mysql-client.yaml
+
+
+kubectl delete -f configmap.yaml
+kubectl delete -f webapp.yaml
+kubectl delete -f webapp_with_vault.yaml
+kubectl delete serviceaccount webapp-sa
